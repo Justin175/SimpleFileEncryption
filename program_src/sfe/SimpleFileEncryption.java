e047e8110bf2b094b74b53a1390868b7c41f860f
Justin Treulieb <justin.treulieb@siemens.com>
2021-01-26 22:03:13 +0100

    Removed dead code

--- program_src/sfe/SimpleFileEncryption.java
+++ program_src/sfe/SimpleFileEncryption.java
@@ -2,11 +2,9 @@ package sfe;
 
 import java.awt.Desktop;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -14,12 +12,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
-import javax.crypto.NoSuchPaddingException;
-
 import sfe.command.ConsoleCommand;
 import sfe.commands.CommandDecrypt;
 import sfe.commands.CommandEncrypt;
-import sfe.io.CryptedInputStream;
 import sfe.utils.FileNameGenerator;
 import sfe.utils.FlagProcessor;
 import sfe.utils.TextUtils;
@@ -304,147 +299,6 @@ public class SimpleFileEncryption {
 		}
 		
 		return true;
-	}
-	
-	private static final boolean decrypt(String[] args) {
-		if(args.length < 3) {
-			setErrorString("Wrong number of Arguments!", "Expected: > 2, Given: " + args.length);
-			return false;
-		}
-		
-		//Check if first argument is a flag
-		FlagType type;
-		if((type = readFlag(args[0])) == FlagType.ERROR) {
-			setErrorString("The first Argument '" + args[0] + "' is not a Password-Flag.");
-			return false;
-		}
-		
-		//read password
-		byte[] password = readPassword(type, args[1]);
-		boolean isHashed = type == FlagType.FILE_HASHED;
-		if(password == null) {
-			return false;
-		}
-		
-		//Check file to encrypt
-		File toDecrypt = new File(args[2]);
-		
-		if(!toDecrypt.exists()) {
-			setErrorString("Decryption-File '" + toDecrypt.getAbsolutePath() + "' not found.");
-			return false;
-		}
-		
-		if(!toDecrypt.isFile()) {
-			setErrorString("Decryption-File-Path '" + toDecrypt.getAbsolutePath() + "' is not linking to a File.", "Expected Type: File, Given: Directory");
-			return false;
-		}
-		
-		//Check dataFile
-		String toWriteInFile;
-		if(args.length >= 4)
-			toWriteInFile = args[3];
-		else {
-			if(toDecrypt.getName().endsWith(".crypted"))
-				toWriteInFile = toDecrypt.getName().substring(0, toDecrypt.getName().length() - ".crypted".length());
-			else
-				toWriteInFile = toDecrypt.getName() + ".decrypted";
-		}
-		File toWriteIn = new File(toWriteInFile);
-		
-		if(toWriteIn.exists() && args.length >= 4) {
-			setErrorString("Goal-File '" + toWriteIn.getAbsolutePath() + "' is already existing.", "Use a other File-Name");
-			return false;
-		}
-		else {
-			//generate next Name
-			toWriteIn = FileNameGenerator.generateValidFile(toWriteIn);
-		}
-		
-		//Create AES-Crypter
-		try {
-			System.out.println("Setup decryption.");
-			long startTime = System.currentTimeMillis();
-			long lastTime = startTime;
-			
-			AESCrypter crypter = new AESCrypter(Crypter.MODE_DECRYPT, password, isHashed);
-			CryptedInputStream is = new CryptedInputStream(new FileInputStream(toDecrypt), crypter);
-			FileOutputStream os = new FileOutputStream(toWriteIn);
-			
-			System.out.print("Decrypting...");
-			int last = is.read();
-			while(last != -1) {
-				os.write(last);
-				last = is.read();
-				
-				if(System.currentTimeMillis() - lastTime > 1000) {
-					System.out.print(".");
-					lastTime = System.currentTimeMillis();
-				}
-			}
-			
-			os.close();
-			is.close();
-			
-			lastTime = System.currentTimeMillis() - startTime;
-			System.out.println();
-			System.out.println("Decryption ends. Time needed: " + (lastTime < 1000 ? lastTime + " ms." : (lastTime / 1000) + " s."));
-		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IOException e) {
-			setErrorString(e.getMessage());
-			return false;
-		}
-		
-		return true;
-	}
-	
-	private static byte[] readPassword(FlagType pwType, String arg) {
-		if(pwType == FlagType.TEXT) //Given in Console
-			return arg.getBytes(StandardCharsets.UTF_8);
-		
-		//Given as File -> read file content
-		File pwFile = new File(arg);
-		if(!pwFile.exists()) {
-			setErrorString("Password-File '" + arg + "' not found.");
-			return null;
-		}
-		
-		if(!pwFile.isFile()) {
-			setErrorString("Password-File-Path is not linking to a File.", "Expected Type: File, Given: Directory");
-			return null;
-		}
-		
-		if(pwFile.length() >= Integer.MAX_VALUE - 100) { //ERROR
-			setErrorString("The Password-File is too big.", "Allowed Bytes: < " + (Integer.MAX_VALUE - 100) + ", Given: " + pwFile.length());
-			return null;
-		}
-		
-		byte[] bytes = new byte[(int) pwFile.length()];
-		int bytesIndex = 0;
-		
-		try {
-			FileInputStream is = new FileInputStream(pwFile);
-			
-			while(bytesIndex != bytes.length) {
-				bytesIndex += is.read(bytes, bytesIndex, bytes.length - bytesIndex);
-			}
-			
-			is.close();
-		} catch (IOException e) {
-			setErrorString("Failed to read Password-File.", "Path=" + pwFile.getAbsolutePath());
-			return null;
-		}
-		
-		return bytes;
-	}
-	
-	private static FlagType readFlag(String fStr) {
-		if(fStr.equals("-p"))
-			return FlagType.TEXT;
-		else if(fStr.equals("-f"))
-			return FlagType.FILE_TEXT;
-		else if(fStr.equals("-h"))
-			return FlagType.FILE_HASHED;
-		else
-			return FlagType.ERROR;
 	}
 	
 	public static void printCommandInfo(ConsoleCommand cmd, String[] allowedFlags, String[] flagsDescription) {
